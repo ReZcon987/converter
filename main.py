78abb0341254f7629ba74a50bc1ee06dbbde4798
rezcon <rezcon@inbox.lv>
2025-09-03 11:22:44 +0300

    v1.1

--- main.py
+++ main.py
@@ -6,3 +6,8 @@ def converterCtoF():
     C = float(input("Ievadi C:"))
     F = (C*9/5)+32
     print (F)
+
+def converterFtoC():
+    F = float(input("Ievadi F:"))
+    C = (F-32)*5/9
+    print (C)
