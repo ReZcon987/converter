fb93b0afc94037a9aa8778695b5b12d62afbdcb1
rezcon <rezcon@inbox.lv>
2025-09-08 11:25:20 +0300

    m2 to ha

--- main.py
+++ main.py
@@ -11,3 +11,9 @@ def converterFtoC():
     F = float(input("Ievadi F:"))
     C = (F-32)*5/9
     print (C)
+
+def convertArea():
+    square_m = int(input("Ievadi kvadrātmetrus: "))
+    ha = square_m/10000
+    print(f"{square_m} kvadrātmetri ir {ha} hektāri.")
+convertArea()
